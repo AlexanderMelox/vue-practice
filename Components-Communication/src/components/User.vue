6af053e49956d7487a7291dc1aed046d8e431e43
Alexander Melo <alexander.melo@ibm.com>
2019-01-28 17:11:10 -0600

    Component communications

---
+++ Components-Communication/src/components/User.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my name</button>
+    <p>Name is {{ name}}</p>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <UserDetail :name="name" @nameWasReset="name = $event" :resetFn="resetName" :userAge="age"></UserDetail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <UserEdit :userAge="age" @ageWasEdited="age = $event"></UserEdit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data() {
+    return {
+      name: "Alex",
+      age: 27
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "Max";
+    },
+    resetName() {
+      this.name = "Alex";
+    }
+  },
+  components: {
+    UserDetail,
+    UserEdit
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ Components-Communication/src/components/UserDetail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>Username: {{ switchName() }}</p>
+    <p>User Age: {{ userAge }}</p>
+    <button @click="resetName">Reset name</button>
+    <button @click="resetFn()">Reset name</button>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main";
+
+export default {
+  props: {
+    name: {
+      type: String
+    },
+    resetFn: Function,
+    userAge: Number
+  },
+  methods: {
+    switchName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName() {
+      this.name = "Alex";
+      this.$emit("nameWasReset", this.name);
+    }
+  },
+  created() {
+    eventBus.$on("ageWasEdited", age => {
+      this.userAge = age;
+    });
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
